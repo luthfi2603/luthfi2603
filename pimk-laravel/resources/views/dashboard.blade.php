1ca968df291deb0bc4f6869cc34f0bfbc46c28a7
luthfi2603 <luthfim904@gmail.com>
2024-04-27 09:58:04 +0700

    tugas 5 pimk

--- pimk-laravel/resources/views/dashboard.blade.php
+++ pimk-laravel/resources/views/dashboard.blade.php
@@ -5,13 +5,31 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+    <div class="absolute top-4 left-0 right-0 mx-auto" id="notif-login">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-60">
+            <div class="bg-white dark:bg-[#111826] overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100 text-center">
+                    Selamat datang {{ auth()->user()->name }}!
                 </div>
             </div>
         </div>
     </div>
+    <div class="flex">
+        <div class="py-12 mx-auto">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg h-10 px-10 py-2">
+                    <a class="text-gray-900 dark:text-gray-100" href="{{ route('tugas.pimk') }}">
+                        Tugas PIMK
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        const notif = document.getElementById("notif-login");
+        
+        setTimeout(() => {
+            notif.classList.add("hidden");
+        }, 1500);
+    </script>
 </x-app-layout>
--- pimk-laravel/routes/web.php
+++ pimk-laravel/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ViewController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('tugas-pimk', [ViewController::class, 'index'])
+    ->name('tugas.pimk');
 
 require __DIR__.'/auth.php';
